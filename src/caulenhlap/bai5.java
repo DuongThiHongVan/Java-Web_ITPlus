d07ff46d729aa28abb283616089f60f0a6b3f568
hongvan <128266763+DuongThiHongVan@users.noreply.github.com>
2024-01-17 09:36:03 +0700

    Update bai5.java

--- src/caulenhlap/bai5.java
+++ src/caulenhlap/bai5.java
@@ -5,11 +5,11 @@ public class bai5 {
 	public static void main(String[] args) {
         for (int i  = 1; i<=100; i++){
             if (i % 3 == 0 && i % 5 == 0)
-                System.out.println("Hello");
+                System.out.println("Hello World");
             else if (i % 5 == 0)
                 System.out.println("World");
             else if ( i % 3 ==0 )
-                System.out.println("Hello word");
+                System.out.println("Hello");
             else
                 System.out.println(i);
 
